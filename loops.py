e914f1707994a2182f3a76bc404ac547ebc92c78
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-14 11:34:18 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,47 @@
+""" loops """
+''' for loop '''
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(0,20,2):
+#     print(i)
+
+# for i in range(1,20,2):
+#     print(i)
+
+# A=["apple","banana","kiwi"]
+# for x in A:
+#     if x=="kiwi":
+#         break
+#     print(x)       
+
+
+''' while loop '''
+
+# i=int(input("enter start number: "))
+# k=int(input("enter  end number: "))
+# while(i<=k):
+#     print(i)
+#     i+=1
+
+
+# i=int(input("enter start number: "))
+# k=int(input("enter  end number: "))
+# while(i<1=k):
+#     print(i)
+#     i-=1
+
+# i=1
+# while(i<=10):
+#     print('hi',i)
+#     i+=1
+
+i=1
+while(i<=10):
+    print('hi',i)
+    if i == 6:
+         break
+    i+=1
+    
+
